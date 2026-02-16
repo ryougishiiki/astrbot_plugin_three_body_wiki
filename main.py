fa6bc8472a063c795b1c9ef52bf2c73e42f33f8f
JSLY <2029872426@qq.com>
2026-02-16 10:50:43 +0800

    Add help command and remove helloworld command

--- main.py
+++ main.py
@@ -11,14 +11,27 @@ class MyPlugin(Star):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
-
-    async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+    @filter.command("santi-help",alias={'三体-帮助'})
+    async def help(self,event:AstrMessageEvent):
+        user_name=event.get_sender_name()
+        from astrbot.api.message_components import Node,Plain
+        help_text=Node(
+            uin=3840638231,
+            name="帮助文档",
+            content=[
+                Plain("灰机三体wiki查询插件帮助列表 \n/santi-help /三体-帮助 ：获取帮助 \n/santi find_thing<str> /三体-查询 find_thing<str> ：在灰机三体wiki中查询 find_thing 内容 <str>类型")
+            ]
+        )
+        yield event.chain_result([help_text])
+    #async def find(self,event:AstrMessageEvent,find_thing:str):
+        
+    #asyncdef helloworld(self, event: AstrMessageEvent):
+    #这是一个 hello world 指令 # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+    #user_name = event.get_sender_name()
+    #message_str = event.message_str # 用户发的纯文本消息字符串
+    #message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
+    #logger.info(message_chain)
+    #yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    
+        async def terminate(self):
+            """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
